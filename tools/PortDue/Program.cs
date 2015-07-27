883f240a9c9e7b152cce50c9a24656dd6e1548cb
Lassi <csharplassi@posteo.de>
2015-07-27 21:51:33 +0200

    Initialer Commit mit funktionieren Linkskript und Makefile

---
+++ tools/PortDue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace PortDue
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length != 1) {
+				throw new ArgumentException ("Port angeben");
+			}
+
+			SerialPort port = new SerialPort (args[0], 1200);
+			port.Open ();
+
+			port.Close ();
+		}
+	}
+}
